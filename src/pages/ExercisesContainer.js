60c1832be726568a18806cc1b956bc5dc7764d79
Jorge Calvo <jorgeecg10@gmail.com>
2020-03-25 17:10:39 -0500

    Adding hooks for ExercisesContainer page

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,48 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Loading from '../components/Loading';
 import Exercises from './Exercises';
 import FatalError from './500';
 
-class ExercisesContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loading: true,
-      error: null,
+const ExercisesContainer = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/api/exercises');
+        const apiData = await res.json();
+        setData(apiData);
+        setLoading(false);
+      } catch (errors) {
+        setLoading(false);
+        setError(errors);
+      }
     };
-  }
+    fetchExercises();
+  }, []);
 
-  async componentDidMount() {
-    await this.fetchExercises();
-  }
+  if (loading) { return <Loading />; }
 
-  fetchExercises = async () => {
-    try {
-      const res = await fetch('http://localhost:8000/api/exercises');
-      const data = await res.json();
-      this.setState({
-        data,
-        loading: false,
-      });
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error,
-      });
-    }
-  }
+  if (error) { return <FatalError />; }
 
-  render() {
-    const { data, loading, error } = this.state;
-
-    if (loading) { return <Loading />; }
-
-    if (error) { return <FatalError />; }
-
-    return <Exercises data={data} />;
-  }
-}
+  return <Exercises data={data} />;
+};
 
 export default ExercisesContainer;
